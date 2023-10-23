82f04ce1978d296abb622e5cda5e34a121a1b082
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2023-10-23 18:20:17 +0200

    created the temporary button to add anime to the list

--- front-end/src/components/quickAction/QuickAction.jsx
+++ front-end/src/components/quickAction/QuickAction.jsx
@@ -1,10 +1,18 @@
 import { MdOutlineAddCircle } from "react-icons/md";
 import style from "./quickAction.module.css";
+import PropTypes from "prop-types";
 
-export default function QuickAction() {
+export default function QuickAction({ color = "black" }) {
     return (
         <div>
-            <MdOutlineAddCircle className={style.buttonWrap} />
+            <MdOutlineAddCircle
+                className={style.buttonWrap}
+                style={{ color: color }}
+            />
         </div>
     );
 }
+
+QuickAction.propTypes = {
+    color: PropTypes.string,
+};
