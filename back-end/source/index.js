33590df95d0cdcee6ad2f4974ec60d507069274a
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2023-10-27 19:19:29 +0200

    backend server initialized

---
+++ back-end/source/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const listRouter = require("./router/list");
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log("Current location: ", req.url);
+    console.log("Current method: ", req.method);
+    next();
+})
+
+app.use("/api/list", listRouter)
+
+app.listen(PORT, () => console.log(`🏃 Server is running on url http://localhost:${PORT}`))
