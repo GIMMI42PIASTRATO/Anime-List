3f9efcd8cd068a01c8f3abfe2a641b903688c291
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2023-11-03 19:20:54 +0100

    Add cors package and enable CORS on server

--- back-end/source/index.js
+++ back-end/source/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const listRouter = require("./router/list");
 const app = express();
+const cors = require("cors")
 
 require("./database")
 
 const PORT = 3000;
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    optionSuccessStatus: 200,
+}))
 
 app.use(express.json());
 app.use((req, res, next) => {
--- front-end/src/components/quickAction/QuickAction.jsx
+++ front-end/src/components/quickAction/QuickAction.jsx
@@ -1,40 +1,32 @@
 import { MdOutlineAddCircle } from "react-icons/md";
 import style from "./quickAction.module.css";
 import PropTypes from "prop-types";
-import { useState, useEffect } from "react";
 
 export default function QuickAction({ color = "black", id }) {
-    const [isClicked, setIsClicked] = useState(false);
-
-    useEffect(() => {
-        async function handleAdd() {
-            try {
-                const response = await fetch(
-                    `http://localhost:3001/api/v1/anime/${id}`,
-                    {
-                        method: "POST",
-                    }
-                );
-                const data = await response.json();
-
-                if (data.status === 500) {
-                    throw new Error(data.msg);
+    const handleAdd = async () => {
+        try {
+            const response = await fetch(
+                `http://localhost:3000/api/list/${id}`,
+                {
+                    method: "POST",
                 }
-            } catch (err) {
-                console.log(err);
+            );
+            const data = await response.json();
+
+            if (data.status === 500) {
+                throw new Error(data.msg);
             }
+        } catch (err) {
+            console.log(err);
         }
-        handleAdd();
-    }, [isClicked]);
+    };
 
     return (
         <div>
             <MdOutlineAddCircle
                 className={style.buttonWrap}
                 style={{ color: color }}
-                onClick={() => {
-                    setIsClicked(true);
-                }}
+                onClick={() => handleAdd()}
             />
         </div>
     );
